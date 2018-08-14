7497c5e5f5412d7a8798b29a4be8d9eb15a94180
Kevin Hwang <mykevin81@gmail.com>
2018-08-14 00:39:22 -0700

    reformat everything

--- ipAddressUpdater/ipBroadcast.go
+++ ipAddressUpdater/ipBroadcast.go
@@ -6,26 +6,33 @@ import (
 	"log"
 	"os"
 	"os/exec"
+
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/sheets/v4"
 )
 
 func main() {
 
 	ipAddress, err := exec.Command("curl", "ipinfo.io/ip").Output()
-	//credentials := getCredentials
-
 	if err != nil {
 		log.Fatal(err)
 		os.Exit(0)
 	}
 
-	fmt.Printf("IP Address: %s\n", ipAddress)
-}
-
-func getCredentials() []byte {
 	cred, err := ioutil.ReadFile("credentials.json")
 	if err != nil {
 		log.Fatalf("Unable to read client secret file: %v", err)
 		os.Exit(0)
 	}
-	return cred
+
+	fmt.Printf("%s\n", cred)
+
+	//config, err := google.ConfigFromJSON(cred, "https://www.googleapis.com/auth/spreadsheets")
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	}
+
+	fmt.Printf("IP Address: %s\n", ipAddress)
 }
